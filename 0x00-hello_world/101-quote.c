f0d67f1d0a932291818b6281961db616807dc08c
hunterlex07 <hunterlex07@gmail.com>
2022-09-11 14:38:09 -0700

    8. UNIX genius -betty length error -Fized?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 - Sucess
+ * Return: Always 0 - Success
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, ", 48, 1, stderr);
+	fwrite("2015-10-19\n", 11, 1, stderr);
 	return (1);
 }
