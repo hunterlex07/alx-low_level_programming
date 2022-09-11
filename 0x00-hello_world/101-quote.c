e85d7eb82b0b404845f01f52fa370acae6773304
hunterlex07 <hunterlex07@gmail.com>
2022-09-11 14:22:14 -0700

    8. UNIX genius

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 - Sucess
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59, 1, stderr);
+	return (1);
+}
