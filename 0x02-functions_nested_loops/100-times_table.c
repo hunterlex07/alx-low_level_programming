c383ee793da473d9a88200f24eacef18bcdea858
hunterlex07 <hunterlex07@gmail.com>
2022-09-14 21:00:25 -0700

    12. The world really does balance

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,7 +30,7 @@ void print_times_table(int n)
 				if (c >= 100)
 				{
 					_putchar((c / 100) + '0');
-					_putchar(((c / 10) % 10) + '0');
+					_putchar(((c / 10)) % 10 + '0');
 				}
 				else if (c <= 99 && c >= 10)
 				{
@@ -38,7 +38,7 @@ void print_times_table(int n)
 				}
 				_putchar((c % 10) + '0');
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
