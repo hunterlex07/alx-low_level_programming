d46f34493c3bebc8aee73a1ca8db00039993630f
hunterlex07 <hunterlex07@gmail.com>
2022-09-14 21:12:43 -0700

    13. The work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - list all natural numbers below 1024 that are multiple of 3 0r 5
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024, i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
