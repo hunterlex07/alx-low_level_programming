677a5bc87d19a8502037284e207a8d25d2e0ae5b
hunterlex07 <hunterlex07@gmail.com>
2022-09-14 20:08:46 -0700

    10. a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - prints the 9 times table, starting with 0.
+ * @n1: the first integer
+ * @n2: the second integer
+ *
+ * Return: the result of the addition
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
