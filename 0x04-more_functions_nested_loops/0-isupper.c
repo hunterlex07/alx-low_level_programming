9540c9f8676c5c8e24439c1dc26a90ee6f704741
hunterlex07 <hunterlex07@gmail.com>
2022-09-16 20:04:16 -0700

    0. isupper -var type fixed!

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,11 +6,10 @@
  *
  * Return: 1 if character is uppercase, 0 if otherwise.
  */
-int _isupper(int c)
+int _isupper(char c)
 {
 	if (c >= 'A' && c <= 'Z')
 		return (1);
-
 	else
 		return (0);
 }
