a90e3ed967a32d8ce572d0e8ac85fe304a208083
hunterlex07 <hunterlex07@gmail.com>
2022-09-20 13:54:59 -0700

    9. strcpy -Fixed null byte?

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,13 +9,13 @@
  *
  * Return: a pointer to the string @dest
  */
-char *_strcpy(char *dest, const char *src)
+char *_strcpy(char *dest, char *src)
 {
 	int index = 0;
 
 	while (src[index])
 	{
-		dest[index] = src[index];
+		dest[index] = src([index] + 1);
 		index++;
 	}
 
