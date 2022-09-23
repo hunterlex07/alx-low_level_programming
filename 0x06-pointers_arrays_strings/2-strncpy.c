78b5f14b8cfc73250bf8a6037fe178683f6b8d05
hunterlex07 <hunterlex07@gmail.com>
2022-09-22 17:16:56 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,13 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int index = 0, src_len = 0;
 
 	while (dest[index++])
 		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+		dest[src_len++] = src[index];
 
 	return (dest);
 }
