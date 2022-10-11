6b1f1558789a0334543dbcb26a68894e5cf16da1
hunterlex07 <hunterlex07@gmail.com>
2022-10-11 14:30:32 -0700

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file the program
+ * is compiled from, followed by a new line.
+ *
+ * Return: always 0.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
