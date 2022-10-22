111e567fc3f0f981d60ab6c185eb969597031b03
hunterlex07 <hunterlex07@gmail.com>
2022-10-21 20:43:36 -0700

    5. The Hare and the Tortoise - fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,10 +3,10 @@
 void __attribute__((constructor)) hare(void);
 
 /**
- * hare - pritns a string before main is executued
+ * hare - prints a string before main is executued
  */
 void hare(void)
 {
-	printf("You're beat! and yet, you must alllow,\n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
